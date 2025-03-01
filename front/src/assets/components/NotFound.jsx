083669205cfa5ccf4583d755fe9865f7bc375066
Kevin Alfaro P <kevin.alfaro290@gmail.com>
2025-03-01 13:37:45 -0300

    create not found page

---
+++ front/src/assets/components/NotFound.jsx
@@ -0,0 +1,26 @@
+import notFoundImage from '../img/404.avif'
+import { Link } from 'react-router-dom'
+
+const NotFound = () => {
+  return (
+    <div className='d-flex justify-content-center align-items-center vh-100 bg-light'>
+      <div className='text-center'>
+        <img
+          src={notFoundImage}
+          alt='Página no encontrada'
+          className='img-fluid mb-4'
+          style={{ maxWidth: '400px' }}
+        />
+        <h1 className='display-4 fw-bold text-dark'>¡Oops! Página no encontrada</h1>
+        <p className='lead text-secondary mb-4'>
+          Parece que la página que estás buscando no existe o ha sido movida.
+        </p>
+        <Link to='/' className='btn btn-primary btn-lg'>
+          Volver al inicio
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default NotFound
