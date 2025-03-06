f6ad8e77db72bee0a4410fa45b2936df7b73bf51
Kevin Alfaro P <kevin.alfaro290@gmail.com>
2025-03-06 17:42:45 -0300

    create index for App

---
+++ front/src/assets/components/index.js
@@ -0,0 +1,5 @@
+export { default as Footer } from './Footer'
+export { default as Header } from './Header'
+export { default as Navigation } from './Navbar'
+export { default as NotFound } from './NotFound'
+export { default as Profile } from './Profile'
---
+++ front/src/assets/pages/index.js
@@ -0,0 +1,5 @@
+export { default as Cart } from './Cart'
+export { default as Home } from './Home'
+export { default as LoginPage } from './LoginPage'
+export { default as RegisterPage } from './RegisterPage'
+export { default as Pizza } from './Pizza'
