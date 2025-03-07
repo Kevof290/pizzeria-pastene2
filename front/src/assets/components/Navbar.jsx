ffb576d58b9111ace20d426caf6df9a9221cd270
Kevin Alfaro P <kevin.alfaro290@gmail.com>
2025-03-07 16:58:38 -0300

    add new context for authentication

--- front/src/assets/components/Navbar.jsx
+++ front/src/assets/components/Navbar.jsx
@@ -3,37 +3,42 @@ import { Link } from 'react-router-dom'
 import { Button, Container, Nav, Navbar, Badge } from 'react-bootstrap'
 import { useContext } from 'react'
 import { CartContext } from '../store/CartContext'
+import { AuthContext } from '../store/AuthContext'
+import '../styles/Navbar.css'
 
 const Navigation = () => {
   const { cart } = useContext(CartContext)
+  const { isAuthenticated, logout } = useContext(AuthContext)
   const total = cart.reduce((acc, { price, count }) => acc + price * count, 0)
   const itemCount = cart.reduce((acc, { count }) => acc + count, 0)
-  const token = false
+
   return (
-    <Navbar bg='dark' variant='dark'>
+    <Navbar bg='dark' variant='dark' expand='md'>
       <Container>
         <Navbar.Brand as={Link} to='/'>Pizzería Pastene
         </Navbar.Brand>
-        <Nav>
-          <Nav.Link as={Link} to='/'>Inicio</Nav.Link>
-          {token
-            ? (
-              <>
-                <p className='btn btn-success mx-1'>🔓 Perfil</p>
-                <p className='btn btn-success mx-1'>🔒 Cerrar Sesión</p>
-              </>
-              )
-            : (
-              <>
-                <Nav.Link as={Link} to='/login'>Iniciar Sesión</Nav.Link>
-                <Nav.Link as={Link} to='/register'>Registrarse</Nav.Link>
-              </>
-              )}
-        </Nav>
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='ms-auto'>
+            <Nav.Link as={Link} to='/'>Inicio</Nav.Link>
+            {isAuthenticated
+              ? (
+                <>
+                  <Nav.Link as={Link} to='/profile'>🔓 Perfil</Nav.Link>
+                  <Nav.Link as={Link} to='/' onClick={logout}>🔒 Cerrar Sesión</Nav.Link>
+                </>
+                )
+              : (
+                <>
+                  <Nav.Link as={Link} to='/login'>Iniciar Sesión</Nav.Link>
+                  <Nav.Link as={Link} to='/register'>Registrarse</Nav.Link>
+                </>
+                )}
+          </Nav>
+        </Navbar.Collapse>
         <div className='d-flex text-end ms-auto'>
-
           <Link to='/cart'>
-            <Button variant='outline-success' className='ms-auto'>Carrito <span>{formatNumber(total)}</span>
+            <Button variant='success' className='ms-auto'>Carrito <span>{formatNumber(total)}</span>
               {itemCount > 0 && (
                 <Badge pill bg='danger' className='ms-2'>
                   {itemCount}
@@ -42,6 +47,7 @@ const Navigation = () => {
             </Button>
           </Link>
         </div>
+
       </Container>
     </Navbar>
   )
