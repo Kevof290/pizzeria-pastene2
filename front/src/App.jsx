2762f5603173bafd9f35251b579777d2e2196129
Kevin Alfaro P <kevin.alfaro290@gmail.com>
2025-03-07 16:57:54 -0300

    add AuthProvider to App

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,22 +2,25 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Cart, Home, LoginPage, RegisterPage, Pizza } from './assets/pages/index'
 import { Header, Profile, NotFound, Navigation, Footer } from './assets/components/index'
 import CartProvider from './assets/store/CartContext'
+import AuthProvider from './assets/store/AuthContext'
 
 const App = () => {
   return (
     <div className='d-flex flex-column min-vh-100'>
       <BrowserRouter>
         <CartProvider>
-          <Navigation />
-          <Routes>
-            <Route path='/' element={<><Header /><Home /></>} />
-            <Route path='/register' element={<RegisterPage />} />
-            <Route path='/login' element={<LoginPage />} />
-            <Route path='/cart' element={<Cart />} />
-            <Route path='/profile' element={<Profile />} />
-            <Route path='/pizza/p001' element={<Pizza />} />
-            <Route path='*' element={<NotFound />} />
-          </Routes>
+          <AuthProvider>
+            <Navigation />
+            <Routes>
+              <Route path='/' element={<><Header /><Home /></>} />
+              <Route path='/register' element={<RegisterPage />} />
+              <Route path='/login' element={<LoginPage />} />
+              <Route path='/cart' element={<Cart />} />
+              <Route path='/profile' element={<Profile />} />
+              <Route path='/pizza/p001' element={<Pizza />} />
+              <Route path='*' element={<NotFound />} />
+            </Routes>
+          </AuthProvider>
         </CartProvider>
         <Footer />
       </BrowserRouter>
