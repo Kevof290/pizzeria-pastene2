29078c2073e4d853dd53962f9c36b4332c0e61b8
Kevin Alfaro P <kevin.alfaro290@gmail.com>
2025-03-07 15:54:13 -0300

    add CartProvider to the top of the App component

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,21 +1,24 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Cart, Home, LoginPage, RegisterPage, Pizza } from './assets/pages/index'
 import { Header, Profile, NotFound, Navigation, Footer } from './assets/components/index'
+import CartProvider from './assets/store/CartContext'
 
 const App = () => {
   return (
     <div className='d-flex flex-column min-vh-100'>
       <BrowserRouter>
-        <Navigation />
-        <Routes>
-          <Route path='/' element={<><Header /><Home /></>} />
-          <Route path='/register' element={<RegisterPage />} />
-          <Route path='/login' element={<LoginPage />} />
-          <Route path='/cart' element={<Cart />} />
-          <Route path='/profile' element={<Profile />} />
-          <Route path='/pizza/p001' element={<Pizza />} />
-          <Route path='*' element={<NotFound />} />
-        </Routes>
+        <CartProvider>
+          <Navigation />
+          <Routes>
+            <Route path='/' element={<><Header /><Home /></>} />
+            <Route path='/register' element={<RegisterPage />} />
+            <Route path='/login' element={<LoginPage />} />
+            <Route path='/cart' element={<Cart />} />
+            <Route path='/profile' element={<Profile />} />
+            <Route path='/pizza/p001' element={<Pizza />} />
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+        </CartProvider>
         <Footer />
       </BrowserRouter>
     </div>
