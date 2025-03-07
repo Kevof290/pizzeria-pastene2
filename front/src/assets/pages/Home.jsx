f8bb2eaa6c5e19f7cbedf7fa34ad4ccc6546391a
Kevin Alfaro P <kevin.alfaro290@gmail.com>
2025-03-07 15:59:41 -0300

    add context for adding items to cart and toast notifications

--- front/src/assets/pages/Home.jsx
+++ front/src/assets/pages/Home.jsx
@@ -1,10 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import CardPizza from '../components/CardPizza'
 import { formatNumber } from '../utils/format'
+import { CartContext } from '../store/CartContext'
 import '../styles/Home.css'
+import { toast, Toaster } from 'sonner'
 
 const Home = () => {
   const [pizzas, setPizzas] = useState([])
+  const { addToCart } = useContext(CartContext)
   const url = 'http://localhost:5000/api/pizzas'
 
   const getPizzas = async () => {
@@ -20,8 +23,14 @@ const Home = () => {
     getPizzas()
   }, [])
 
+  const handleAddToCart = (pizza) => {
+    addToCart(pizza)
+    toast.info(`${pizza.name} ha sido agregado al carrito`)
+  }
+
   return (
     <main className='container my-3'>
+      <Toaster position='bottom-right' expand />
       <div className='row row-cols-1 row-cols-md-6 g-2'>
         {pizzas.map((pizza) => (
           <CardPizza
@@ -33,6 +42,7 @@ const Home = () => {
             name={pizza.name}
             price={formatNumber(pizza.price)}
             className='card h-100 shadow-sm mb-3'
+            addToCart={() => handleAddToCart(pizza)}
           />
         ))}
       </div>
