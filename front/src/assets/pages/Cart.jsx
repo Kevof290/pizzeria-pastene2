ad64ec480d65763cb1395b160e37192fda8ad09d
Kevin Alfaro P <kevin.alfaro290@gmail.com>
2025-03-07 15:56:48 -0300

    add context to Cart

--- front/src/assets/pages/Cart.jsx
+++ front/src/assets/pages/Cart.jsx
@@ -1,58 +1,40 @@
-import React, { useState } from 'react'
-import { pizzaCart } from '../utils/pizzas'
+import React, { useContext } from 'react'
+import { CartContext } from '../store/CartContext'
 import { formatNumber } from '../utils/format'
 import '../styles/Cart.css'
 
 const Cart = () => {
-  const [cart, setCart] = useState(pizzaCart)
-
-  const more = (id) => {
-    setCart(cart.map((pizza) => {
-      if (pizza.id === id) {
-        return { ...pizza, count: pizza.count + 1 }
-      }
-      return pizza
-    }))
-  }
-
-  const less = (id) => {
-    setCart((prevCart) =>
-      prevCart.map((pizza) => {
-        if (pizza.id === id) {
-          return { ...pizza, count: pizza.count - 1 }
-        }
-        return pizza
-      })
-        .filter((pizza) => pizza.count > 0)
-    )
-  }
+  const { cart, increaseCount, decreaseCount } = useContext(CartContext)
 
   const total = cart.reduce((acc, { price, count }) => acc + price * count, 0)
 
   return (
-    <div className='container-fluid d-flex flex-column min-vh-100'>
-      <div className='list-group list-group-flush my-3 mx-auto w-75'>
-        {cart.map(({ id, name, price, count, img }) => (
-          <div
-            className='list-group-item d-flex align-items-center justify-content-between'
-            key={id}
-          >
-            <div className='d-flex align-items-center'>
-              <img src={img} className='cart' alt={name} />
-              <h3 className='mb-0'>{name}</h3>
-            </div>
-            <div className='d-flex align-items-center'>
-              <p className='mb-0 me-3'>{formatNumber(price)}</p>
-              <button className='btn btn-outline-secondary' onClick={() => less(id)}>-</button>
-              <p className='mx-3 mb-0'>{count}</p>
-              <button className='btn btn-outline-secondary' onClick={() => more(id)}>+</button>
-            </div>
-          </div>
-        ))}
-        <div className='my-5 text-end'>
-          <h3>Total: {formatNumber(total)}</h3>
-          <button className='btn btn-success'>Pagar</button>
-        </div>
+    <div className='container mt-5'>
+      <h2>Carrito de Compras</h2>
+      {cart.length === 0
+        ? (
+          <p>No hay elementos en el carrito.</p>
+          )
+        : (
+          <ul className='list-group'>
+            {cart.map((pizza) => (
+              <li key={pizza.id} className='list-group-item d-flex justify-content-between align-items-center'>
+                <img src={pizza.img} className='cart rounded-1' alt={pizza.name} />
+                <div className='fs-5'>
+                  {pizza.name.toUpperCase()} - {formatNumber(pizza.price)}
+                </div>
+                <div>
+                  <button className='btn btn-sm btn-danger mx-2' onClick={() => decreaseCount(pizza.id)}>-</button>
+                  <span className='fs-5'>{pizza.count}</span>
+                  <button className='btn btn-sm btn-success mx-2' onClick={() => increaseCount(pizza.id)}>+</button>
+                </div>
+              </li>
+            ))}
+          </ul>
+          )}
+      <div className='my-5 text-end'>
+        <h3>Total: {formatNumber(total)}</h3>
+        <button className='btn btn-success'>Pagar</button>
       </div>
     </div>
   )
