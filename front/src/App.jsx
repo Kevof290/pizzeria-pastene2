5dec7fdd842185c184789a216515d1fcf6b38a65
Kevin Alfaro P <kevin.alfaro290@gmail.com>
2025-03-14 23:59:27 -0300

    Cleaning App

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,28 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Cart, Home, LoginPage, RegisterPage, Pizza } from './assets/pages/index'
-import { Header, Profile, NotFound, Navigation, Footer } from './assets/components/index'
-import CartProvider from './assets/store/CartContext'
-import AuthProvider from './assets/store/AuthContext'
-import { ProtectedRoute, ReverseProtectedRoute } from './assets/components/ProtectedRoute'
+import { BrowserRouter } from 'react-router-dom'
+import { Navigation, Footer } from './assets/components/index'
+import AppRoutes from './assets/routes/AppRoutes'
+import AppContext from './assets/routes/AppContext'
 
 const App = () => {
   return (
     <div className='d-flex flex-column min-vh-100'>
       <BrowserRouter>
-        <CartProvider>
-          <AuthProvider>
-            <Navigation />
-            <Routes>
-              <Route path='/' element={<><Header /><Home /></>} />
-              <Route path='/register' element={<ReverseProtectedRoute><RegisterPage /></ReverseProtectedRoute>} />
-              <Route path='/login' element={<ReverseProtectedRoute><LoginPage /></ReverseProtectedRoute>} />
-              <Route path='/cart' element={<Cart />} />
-              <Route path='/profile' element={<ProtectedRoute><Profile /></ProtectedRoute>} />
-              <Route path='/pizza/:id' element={<Pizza />} />
-              <Route path='*' element={<NotFound />} />
-            </Routes>
-          </AuthProvider>
-        </CartProvider>
+        <AppContext>
+          <Navigation />
+          <AppRoutes />
+        </AppContext>
         <Footer />
       </BrowserRouter>
     </div>
