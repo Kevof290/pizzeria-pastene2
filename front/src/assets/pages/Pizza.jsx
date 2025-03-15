0425a945080ca4d6212f6029f162de0bcc1db0b3
Kevin Alfaro P <kevin.alfaro290@gmail.com>
2025-03-14 23:37:28 -0300

    add useParams to render according to id. Structuring card

--- front/src/assets/pages/Pizza.jsx
+++ front/src/assets/pages/Pizza.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { Button, Card, Col, Container, Row } from 'react-bootstrap'
+import '../styles/Pizza.css'
 
 const Pizza = () => {
   const [onePizza, setOnePizza] = useState([])
-  const url = 'http://localhost:5000/api/pizzas/p001'
+  const { id } = useParams()
+  const url = `http://localhost:5000/api/pizzas/${id}`
   const formattedPrice = new Intl.NumberFormat('es-CL', { style: 'currency', currency: 'CLP' }).format(onePizza.price)
 
   const getOnePizza = async () => {
@@ -16,22 +20,32 @@ const Pizza = () => {
   }
   useEffect(() => {
     getOnePizza()
-  }, [])
+  }, [id])
   return (
-    <main className='container my-3 mb-3'>
-      <div className='row row-cols-2 g-2 d-flex flex-column align-items-center'>
-        <div className='card h-100 shadow-sm'>
-          <img className='card-body' src={onePizza.img} alt={onePizza.name} />
-          <h1 className='card-title mx-auto'>{onePizza.name}</h1>
-          <p className='card-body'>{onePizza.desc}</p>
-          <p className='card-body fs-5'>{onePizza.ingredients}</p>
-          <p className='card-body fs-3'>{formattedPrice}</p>
-          <div className='card-footer text-end'>
-            <button className='footer btn btn-success my-2 mx-2'>Comprar</button>
-          </div>
-        </div>
-      </div>
-    </main>
+    <Container className='my-3'>
+      <Row className='justify-content-center'>
+        <Col md={8}>
+          <Card className='shadow-sm'>
+            <Row noGutters>
+              <Col md={4}>
+                <Card.Img className='img-pizza' variant='top' src={onePizza.img} alt={onePizza.name} />
+              </Col>
+              <Col md={8}>
+                <Card.Body>
+                  <Card.Title>{onePizza.name}</Card.Title>
+                  <Card.Text>{onePizza.desc}</Card.Text>
+                  <Card.Text>
+                    <strong>Ingredientes:</strong> {onePizza.ingredients}
+                  </Card.Text>
+                  <Card.Text className='fs-3'>{formattedPrice}</Card.Text>
+                  <Button variant='success'>Comprar</Button>
+                </Card.Body>
+              </Col>
+            </Row>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
